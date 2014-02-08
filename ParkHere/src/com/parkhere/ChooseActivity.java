3c8b7a95d567578efce23b7bc8031aff5eb96ebe
Keven Villeneuve <kev.v@hotmail.com>
2014-02-08 18:25:13 -0500

    add choose activity

--- ParkHere/src/com/parkhere/ChooseActivity.java
+++ ParkHere/src/com/parkhere/ChooseActivity.java
@@ -2,7 +2,12 @@ package com.parkhere;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ChooseActivity extends Activity {
 
@@ -10,6 +15,13 @@ public class ChooseActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_choose);
+		
+		ImageView img = (ImageView) findViewById(R.id.imageView1);
+		img.setOnClickListener(new OnClickListener() {
+		    public void onClick(View v) {
+		    	//TODO:
+		    }
+		});
 	}
 
 	@Override
--- ParkHere/src/com/parkhere/MainActivity.java
+++ ParkHere/src/com/parkhere/MainActivity.java
@@ -1,7 +1,16 @@
 package com.parkhere;
 
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +19,34 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		/*Parse.initialize(this, "rHQ443EdBXs3eySuO5Dls9wA8abPBPWTnS5CUlNe", "h4bMXEvZOwfjlCKnRrA8y8YZHhgF8OpMu57FaZDo");
+		
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
+		
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("GameScore");
+		query.whereEqualTo("playerName", "Dan Stemkoski");
+		query.findInBackground(new FindCallback<ParseObject>() {
+		    public void done(List<ParseObject> scoreList, ParseException e) {
+		        if (e == null) {
+		            //OK
+		        } else {
+		            //ERROR
+		        }
+		    }
+
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});*/
+		
+		/*Intent i = new Intent(MainActivity.this, ChooseActivity.class);
+        startActivity(i);
+        finish();*/
 	}
 
 	@Override
