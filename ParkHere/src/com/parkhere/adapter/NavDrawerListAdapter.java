103c6dc3f8714f8b41f1a83495ae7174b10faca8
miguel.nieto@gmail.com <miguel@plix.co>
2014-02-08 20:56:15 -0500

    menu

--- ParkHere/src/com/parkhere/adapter/NavDrawerListAdapter.java
+++ ParkHere/src/com/parkhere/adapter/NavDrawerListAdapter.java
@@ -41,12 +41,20 @@ public class NavDrawerListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		
 		if (convertView == null) {
             LayoutInflater mInflater = (LayoutInflater)
                     context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-            convertView = mInflater.inflate(R.layout.drawer_list_item, null);
-        }
-         
+
+            
+    		if(position == 0 || position == 3 || position == 6){
+                convertView = mInflater.inflate(R.layout.drawer_list_item_white, null);    			
+    		}else{
+    			convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+    		}
+
+		}
+		
 //        ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
         TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
  //       TextView txtCount = (TextView) convertView.findViewById(R.id.counter);
