0e7116f406011ff8f43296f311eaf6ac1f0c78d5
miguel.nieto@gmail.com <miguel@plix.co>
2014-02-08 23:14:21 -0500

    menu fix

--- ParkHere/src/com/parkhere/adapter/NavDrawerListAdapter.java
+++ ParkHere/src/com/parkhere/adapter/NavDrawerListAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.parkhere.R;
@@ -48,39 +49,21 @@ public class NavDrawerListAdapter extends BaseAdapter {
 
             
     		if(position == 0 || position == 3 || position == 6){
-                convertView = mInflater.inflate(R.layout.drawer_list_item_white, null);    			
+                convertView = mInflater.inflate(R.layout.drawer_list_item_white, null);
+    			ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
+    			imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
+    			TextView textView = (TextView)convertView.findViewById(R.id.title);
+    			textView.setText(navDrawerItems.get(position).getTitle());	
+
     		}else{
     			convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+    			TextView textView = (TextView)convertView.findViewById(R.id.title);
+    			textView.setText(navDrawerItems.get(position).getTitle());	
     		}
+			
+			
 
 		}
-        TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
-        txtTitle.setText("test");
-
-		if(position==0){
-			ImageView imgIcon = (ImageView) convertView.findViewById(R.drawable.car);
-		}else if(position == 3){
-			ImageView imgIcon = (ImageView) convertView.findViewById(R.drawable.cat2);
-		}else if(position == 6){
-			ImageView imgIcon = (ImageView) convertView.findViewById(R.drawable.cat3);
-		}else{
-	        txtTitle.setText(navDrawerItems.get(position).getTitle());
-		}
-		
- //       TextView txtCount = (TextView) convertView.findViewById(R.id.counter);
-         
-   //     imgIcon.setImageResource(navDrawerItems.get(position).getIcon());        
-        
-        // displaying count
-        // check whether it set visible or not
-   /*
-        if(navDrawerItems.get(position).getCounterVisibility()){
-        	txtCount.setText(navDrawerItems.get(position).getCount());
-        }else{
-        	// hide the counter view
-        	txtCount.setVisibility(View.GONE);
-        }
-     */   
         return convertView;
 	}
 
